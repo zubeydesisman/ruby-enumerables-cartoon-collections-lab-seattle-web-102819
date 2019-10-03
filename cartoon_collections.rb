3242ddc83d8afb34cf68d4069589648658c88b5a
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-03 02:08:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ end
 # end
 def find_the_cheese(array)
    cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.all? {|i| cheese_types.include?(i)}
+  if array.find {|i| cheese_types.include?(i)}
     return cheese_types & array
 end
 
